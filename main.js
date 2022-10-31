c76664fcc8bc59377b63bd3aa0b23a820ec70db3
Vitor Moraes <vitoristvan@hotmail.com>
2022-10-31 15:47:09 -0300

    api check equals

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const apiZooAnimal = 'https://zoo-animal-api.herokuapp.com/animals/rand/';
 
 async function loadAnimals(){
-    let arrayPromisses = [];
     let arrayZoo = [];
 
-    for (let i = 0; i < 10; i++) {  
-        const res = fetch(apiZooAnimal);
-        arrayPromisses.push(res);
+    for (let i = 0; i < 50; i++) {  
+        const res = await fetch(apiZooAnimal);
+        const data = await res.json();
+        let state = true;
+
+        if(arrayZoo.length != 0){
+            arrayZoo.forEach(element => {
+                if(element.name === data.name){
+                    i--;
+                    state = false;
+                }; 
+            })
+            
+        }
+
+        if(state === true){arrayZoo.push(data)};       
     }
 
-    const responses = await Promise.all(arrayPromisses);
-    const data = await Promise.all(responses.map(element => element.json()));
-    arrayZoo.push(data);
+console.log(arrayZoo);
+        
 }
+
 
 loadAnimals();
